34afe425c7ae26a7183e022e7b8fa7563cd6e0b0
Nahuel Angelinetti <nahuel@ahtna.org>
2012-05-18 18:16:13 +0200

    PEP-8 support

--- preylog/models.py
+++ preylog/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 
+
 class API_User(models.Model):
     username = models.CharField(max_length=255)
 
+
 class Device(models.Model):
-    user       = models.ForeignKey(API_User, related_name="devices")
-    key        = models.CharField(max_length=255)
-    lost       = models.BooleanField(default=False)
-    title      = models.CharField(max_length=255)
+
+    user = models.ForeignKey(API_User, related_name="devices")
+    key = models.CharField(max_length=255)
+    lost = models.BooleanField(default=False)
+    title = models.CharField(max_length=255)
     os_version = models.CharField(max_length=255)
-    os         = models.CharField(max_length=255)
-    genre      = models.CharField(max_length=255)
+    os = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+
 
 class Report(models.Model):
-    device     = models.ForeignKey(Device, related_name="reports")
-    data       = models.TextField()
+    device = models.ForeignKey(Device, related_name="reports")
+    data = models.TextField()
+
 
 class ReportFile(models.Model):
-    report     = models.ForeignKey(Report, related_name="files")
-    filename   = models.FileField(upload_to = "files")
+    report = models.ForeignKey(Report, related_name="files")
+    filename = models.FileField(upload_to="files")
--- preylog/views.py
+++ preylog/views.py
@@ -7,104 +7,108 @@ import hashlib
 import uuid
 import base64
 
+
 @csrf_exempt
 def log(request):
-	print "log"
-	print request.POST
-	print request.GET
-	print request.FILES
-	return HttpResponse()
+    print "log"
+    print request.POST
+    print request.GET
+    print request.FILES
+    return HttpResponse()
+
 
 @csrf_exempt
 def devices(request):
-	print "devicess"
+    print "devicess"
 
-	user = authenticate(request)
+    user = authenticate(request)
 
-	try:
-		device, new = Device.objects.get_or_create(
-										title = request.POST['device[title]'], 
-										os_version = request.POST['device[os_version]'],
-										os = request.POST['device[os]'],
-										genre = request.POST['device[device_type]'],
-										user = user,
-									)
-		if new:
-			device.key = uuid.uuid4()
-			device.save()
-	except Exception, e:
-		print e
-	reponse = '<devices type="array"><device><key>9xy9ne</key><state>ok</state><title>yaya</title></device></devices>'
-	r = HttpResponse('<key>%s</key>' % device.key)
-	r.status_code = 201
-	return r
+    try:
+        device, new = Device.objects.get_or_create(
+                                        title=request.POST['device[title]'],
+                                        os_version=request.POST['device[os_version]'],
+                                        os=request.POST['device[os]'],
+                                        genre=request.POST['device[device_type]'],
+                                        user=user,
+                                        )
+        if new:
+            device.key = uuid.uuid4()
+            device.save()
+    except Exception, e:
+        print e
+    reponse = '<devices type="array"><device><key>9xy9ne</key><state>ok</state>'\
+              + '<title>yaya</title></device></devices>'
+    r = HttpResponse('<key>%s</key>' % device.key)
+    r.status_code = 201
+    return r
+
 
 @csrf_exempt
 def device(request, key):
-	print "device"
+    print "device"
 
-	user = authenticate(request)
+    user = authenticate(request)
 
-	try:
-		device = Device.objects.get(key = key)
+    try:
+        device = Device.objects.get(key=key)
 
-		if request.method == "PUT":
-			report_data = request.read()
-			report = Report( device = device, data = report_data )
-			report.save()
-			return HttpResponse()
+        if request.method == "PUT":
+            report_data = request.read()
+            report = Report(device=device, data=report_data)
+            report.save()
+            return HttpResponse()
 
+        if device.lost == True:
+            response = "<device>\n<status>\n<missing>%s</missing>\n</status>" % device.lost \
+                        + "<configuration>\n<current_release>0.5.3</current_release>\n" \
+                        + "<post_url>http://localhost/report/%s.xml</post_url>\n" % device.key \
+                        + "<delay>20</delay>\n<auto_update>true</auto_update>\n</configuration>\n" \
+                        + "<modules>\n" + '<module type="action" active="true" name="system" version="1.5"/>' + "\n" \
+                        + '<module type="action" active="true" name="webcam" version="1.5"/>\n</modules>' \
+                        + "\n</device>"
 
-		if device.lost == True:
-			response = "<device>\n<status>\n<missing>%s</missing>\n</status>" % device.lost \
-						+ "<configuration>\n<current_release>0.5.3</current_release>\n" \
-						+ "<post_url>http://localhost/report/%s.xml</post_url>\n" % device.key \
-						+ "<delay>20</delay>\n<auto_update>true</auto_update>\n</configuration>\n" \
-						+ "<modules>\n"+'<module type="action" active="true" name="system" version="1.5"/>' + "\n" \
-						+ '<module type="action" active="true" name="webcam" version="1.5"/>\n</modules>' \
-						+ "\n</device>"
+            return HttpResponseNotFound(response)
+        else:
+            response = '<device><status><missing>%s</missing></status>' % device.lost \
+                        + '<configuration><current_release>0.5.3</current_release>' \
+                        + '<delay>20</delay><auto_update>true</auto_update></configuration>' \
+                        + '<modules><module type="action" active="true" name="system" version="1.5"/>' \
+                        + '<module type="action" active="true" name="webcam" version="1.5"/></modules></device>'
+            return HttpResponse(response)
+    except Exception, e:
+        print e
 
-			return HttpResponseNotFound(response)
-		else:
-			response = '<device><status><missing>%s</missing></status>' % device.lost \
-						+ '<configuration><current_release>0.5.3</current_release>' \
-						+ '<delay>20</delay><auto_update>true</auto_update></configuration>' \
-						+ '<modules><module type="action" active="true" name="system" version="1.5"/>' \
-						+ '<module type="action" active="true" name="webcam" version="1.5"/></modules></device>'
-			return HttpResponse(response)
-	except Exception, e:
-		print e
-	
-	return HttpResponse()
+    return HttpResponse()
+
 
 @csrf_exempt
 def report(request, key):
-	print "report"
-	
-	user = authenticate(request)
+    print "report"
 
+    user = authenticate(request)
 
-	try:
-		device = Device.objects.get(key = key)
-		report = Report( device = device , data = request.POST)
-		report.save()
-		print request.FILES
-		for report_file, data_file in request.FILES.items():
-			new_file = ReportFile(report = report)
-			new_file.save()
-			new_file.filename.save( key + "-" + data_file, ContentFile(request.FILES[report_file].read()))
-	except Exception, e:
-		print e
-	
-	return HttpResponse()
+    try:
+        device = Device.objects.get(key=key)
+        report = Report(device=device, data=request.POST)
+        report.save()
+        print request.FILES
+        for report_file, data_file in request.FILES.items():
+            new_file = ReportFile(report=report)
+            new_file.save()
+            new_file.filename.save(key + "-" + data_file, ContentFile(request.FILES[report_file].read()))
+    except Exception, e:
+        print e
+
+    return HttpResponse()
+
 
 def authenticate(request):
-	try:
-		if 'HTTP_AUTHORIZATION' in request.META:
-			auth = request.META['HTTP_AUTHORIZATION'].split()
-			username, passwd = base64.b64decode(auth[1]).split(':')
-			user = API_User.objects.get(username = str(username))
-			return user
+    try:
+        if 'HTTP_AUTHORIZATION' in request.META:
+            auth = request.META['HTTP_AUTHORIZATION'].split()
+            username, passwd = base64.b64decode(auth[1]).split(':')
+            user = API_User.objects.get(username=str(username))
+            return user
 
-	except Exception, e:
-		print e
+    except Exception, e:
+        print e
